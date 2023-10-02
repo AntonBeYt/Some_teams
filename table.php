acd116334021c6e3f2affcb7462a8c123e108f1f
AntonBeYt <143318322+AntonBeYt@users.noreply.github.com>
2023-10-02 08:52:04 +0200

    getting somewhere on table

--- table.php
+++ table.php
@@ -2,11 +2,18 @@
 require __DIR__ . ('/data.php');
 ?>
 <table>
-    <?php foreach ($teams as $team) {
-        echo "<tr>";
-        foreach ($team as $column) {
-            echo "<td>$column</td>";
-        }
-        echo "</tr>";
-    }
-    echo "</table>";
+    <?php foreach ($teams as $key => $team) : ?>
+        <tr>
+            <th><?= $key ?></th>
+            <td><?= $team['league']; ?></td>
+            <td><?= $team['last-time-champions'] ?></td>
+            <td><?= $team['city'] ?></td>
+            <td><?php if (isset($team['nickname'])) {
+                    echo $team['nickname'];
+                } else {
+                    echo "-";
+                } ?></td>
+            <td><?= $team['url'] ?></td>
+        </tr>
+    <?php endforeach;  ?>
+</table>
