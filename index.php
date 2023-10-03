b8bf0510c7bcb62e4416bbe1177fe81466f704fe
AntonBeYt <143318322+AntonBeYt@users.noreply.github.com>
2023-10-03 07:55:54 +0200

    finished table and styled it some

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
 require __DIR__ . ('/header.php');
 
+require __DIR__ . ('/table.php'); ?>
+<table>
+    <tr>
+        <th>
+            <h3>Total number of teams:</h3>
+        </th>
+        <td>
+            <?php require __DIR__ . ('/totalTeams.php'); ?>
+        </td>
+    </tr>
+</table>
 
 
-require __DIR__ . ('/footer.php');
+<?php require __DIR__ . ('/footer.php'); ?>
--- table.php
+++ table.php
@@ -2,6 +2,14 @@
 require __DIR__ . ('/data.php');
 ?>
 <table>
+    <tr>
+        <td class="t-head">Team</td>
+        <td class="t-head">League</td>
+        <td class="t-head">Last championship win</td>
+        <td class="t-head">City</td>
+        <td class="t-head">Nickname</td>
+        <td class="t-head">Homepage</td>
+    </tr>
     <?php foreach ($teams as $key => $team) : ?>
         <tr>
             <th><?= $key ?></th>
