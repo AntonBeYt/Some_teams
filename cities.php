5399aa61ee927ca6d617c4621a52ce8969d7e066
AntonBeYt <143318322+AntonBeYt@users.noreply.github.com>
2023-10-03 08:30:43 +0200

    sorted cities and removed duplicates

---
+++ cities.php
@@ -0,0 +1,11 @@
+<?php
+require __DIR__ . ('/data.php');
+
+foreach ($teams as $team) :
+    $cities[] = $team['city'];
+endforeach;
+
+$cities = array_unique($cities);
+foreach ($cities as $city) :
+    echo $city;
+endforeach;
